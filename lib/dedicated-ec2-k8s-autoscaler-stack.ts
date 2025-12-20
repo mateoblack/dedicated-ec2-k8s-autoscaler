b7a58fbe457304a4dc760fc4598054ce125396b5
Matthew  Black <matoblac@amazon.com>
2025-12-19 19:58:33 -0500

    created vpc cidrs tests=passing

--- lib/dedicated-ec2-k8s-autoscaler-stack.ts
+++ lib/dedicated-ec2-k8s-autoscaler-stack.ts
@@ -1,10 +1,51 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class DedicatedEc2K8sAutoscalerStack extends cdk.Stack {
+  public readonly vpc: ec2.Vpc;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Add your resources here
+    this.vpc = new ec2.Vpc(this, 'DedicatedVpc', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      defaultInstanceTenancy: ec2.DefaultInstanceTenancy.DEDICATED,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'ControlPlane',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+        {
+          cidrMask: 24,
+          name: 'DataPlane',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+        {
+          cidrMask: 24,
+          name: 'Management',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        }
+      ],
+      maxAzs: 3
+    });
+
+    // Add secondary CIDR for pod communication
+    const secondaryCidr = new ec2.CfnVPCCidrBlock(this, 'PodCommunicationCidr', {
+      vpcId: this.vpc.vpcId,
+      cidrBlock: '10.1.0.0/16'
+    });
+
+    // Create subnets in secondary CIDR for pod communication
+    const azs = cdk.Stack.of(this).availabilityZones.slice(0, 3);
+    azs.forEach((az, index) => {
+      new ec2.CfnSubnet(this, `PodCommunicationSubnet${index + 1}`, {
+        vpcId: this.vpc.vpcId,
+        cidrBlock: `10.1.${index}.0/24`,
+        availabilityZone: az,
+        tags: [{ key: 'Name', value: `PodCommunication-${index + 1}` }]
+      }).addDependency(secondaryCidr);
+    });
   }
 }
--- test/dedicated-ec2-k8s-autoscaler.test.ts
+++ test/dedicated-ec2-k8s-autoscaler.test.ts
@@ -1,11 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as DedicatedEc2K8sAutoscaler from '../lib/dedicated-ec2-k8s-autoscaler-stack';
 
-test('Stack creates successfully', () => {
+test('VPC with dedicated tenancy and CIDR configuration', () => {
   const app = new cdk.App();
-  const stack = new DedicatedEc2K8sAutoscaler.DedicatedEc2K8sAutoscalerStack(app, 'MyTestStack');
+  const stack = new DedicatedEc2K8sAutoscaler.DedicatedEc2K8sAutoscalerStack(app, 'TestStack');
   const template = Template.fromStack(stack);
 
-  expect(template).toBeDefined();
+  // Test VPC with primary CIDR and dedicated tenancy
+  template.hasResourceProperties('AWS::EC2::VPC', {
+    CidrBlock: '10.0.0.0/16',
+    InstanceTenancy: 'dedicated'
+  });
+
+  // Test secondary CIDR block for pod communication
+  template.hasResourceProperties('AWS::EC2::VPCCidrBlock', {
+    CidrBlock: '10.1.0.0/16'
+  });
+
+  // Test total subnets (6 primary + 2 pod communication = 8, limited by AZs)
+  template.resourceCountIs('AWS::EC2::Subnet', 8);
+
+  // Test primary subnet CIDRs
+  template.hasResourceProperties('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.0.0/24'
+  });
+  template.hasResourceProperties('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.1.0/24'
+  });
+
+  // Test pod communication subnet CIDRs
+  template.hasResourceProperties('AWS::EC2::Subnet', {
+    CidrBlock: '10.1.0.0/24'
+  });
+  template.hasResourceProperties('AWS::EC2::Subnet', {
+    CidrBlock: '10.1.1.0/24'
+  });
 });
